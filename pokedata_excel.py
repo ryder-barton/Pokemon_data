c04651d3bc138c2458ac27bd2454eac129f16b98
Ryder Barton <155204102+ryder-barton@users.noreply.github.com>
2024-01-01 17:08:07 -0600

    Add files via upload

---
+++ pokedata_excel.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import requests
+from tabulate import tabulate
+import pandas as pd
+
+def get_pokemon_info():
+    base_url = 'https://pokeapi.co/api/v2/pokemon/'  
+    data = []
+
+    for number in range(1, 152):  
+        url = base_url + str(number)
+        response = requests.get(url)
+
+        id = response.json()['id']
+        name = response.json()['name'].title()
+        types = ', '.join([t['type']['name'] for t in response.json()['types']])
+        height = round(response.json()['height'] / 10 * 3.28084, 2)
+        weight = round(response.json()['weight'] / 10 * 2.20462, 2)
+
+        data.append([f'#{id}', name, types, height, weight])
+
+    headers = ["ID", "Name", "Types", "Height(ft)", "Weight(lbs)"]
+
+    df = pd.DataFrame(data, columns=headers)
+
+    print(tabulate(df, headers=headers, tablefmt="grid"))
+
+    df.to_excel('pokedata.xlsx', index=False)
+
+get_pokemon_info()
